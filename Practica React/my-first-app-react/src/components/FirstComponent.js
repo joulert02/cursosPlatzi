0383f4793f7e02edb2a4e156814d0735ec431580
Jouler Pragma <jouler.talaigua@pragma.com.co>
2019-10-21 19:01:39 -0500

    create base project

---
+++ Practica React/my-first-app-react/src/components/FirstComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class FirstComponent extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Jouler Taigua</h1>
+                <h2>Practicando React</h2>
+            </React.Fragment>
+        )
+    }
+}
+
+export default FirstComponent
